487eaac35eb5d0353f1add7a622c8fef89b5aee3
thanhthanhthile <thanhthanh08022001@gmail.com>
2021-06-30 18:35:42 +0700

    Change for Driver in UpdateProfile

--- app/src/main/java/com/example/icar/model/Driver.java
+++ app/src/main/java/com/example/icar/model/Driver.java
@@ -8,9 +8,8 @@ public class Driver {
     public String address;
     public String birthday;
     public boolean gender;
-    public String license_type;
 
-    public Driver(String uid, String full_name, String email, String phone, String address, String birthday, boolean gender, String license_type) {
+    public Driver(String uid, String full_name, String email, String phone, String address, String birthday, boolean gender) {
         this.uid = uid;
         this.full_name = full_name;
         this.email = email;
@@ -18,23 +17,9 @@ public class Driver {
         this.address = address;
         this.birthday = birthday;
         this.gender = gender;
-        this.license_type = license_type;
-    }
-
-    @Override
-    public String toString() {
-        return "Driver{" +
-                "uid='" + uid + '\'' +
-                ", full_name='" + full_name + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                ", address='" + address + '\'' +
-                ", birthday='" + birthday + '\'' +
-                ", gender='" + gender + '\'' +
-                ", license_type=" + license_type +
-                '}';
     }
 
     public Driver() {
+
     }
 }
